7b62665233a96dfad6c09ef1739f95eb126c6cd5
Sanel Zukan <karijes@users.sf.net>
2006-07-23 22:31:56 +0000

    Removed silly checkers (bad idea). Added brief info about X errors. Cleaned Utils.h,cpp.

--- Utils.cpp
+++ Utils.cpp
@@ -13,32 +13,6 @@
 #include "Utils.h"
 #include <efltk/x.h>
 
-int x_return_code = 0;
-
-void* GetProperty(Window w, Atom a, Atom type, unsigned long *np, int *ret)
-{
-	Atom realType;
-	int format;
-	unsigned long n, extra;
-	unsigned char *prop=0;
-	int status = XGetWindowProperty(fl_display, w, a, 0L, 0x7fffffff,
-		False, type, &realType, &format, &n, &extra, (uchar**)&prop);
-	if(ret) 
-		*ret = status;
-	if(status != Success) 
-		return 0;
-	if(!prop) 
-		return 0;
-	if(!n)
-   	{
-		XFree(prop); 
-		return 0; 
-	}
-	if(np) 
-		*np = n;
-	return prop;
-}
-
 void SendMessage(Window win, Atom a, Atom l)
 {
 	XEvent ev;
@@ -53,22 +27,3 @@ void SendMessage(Window win, Atom a, Atom l)
 	mask = 0L;
 	XSendEvent(fl_display, win, False, mask, &ev);
 }
-
-/*
-void SetProperty(Window w, Atom a, Atom type, unsigned long val)
-{
-	XChangeProperty(fl_display, w, a, type, 32, PropModeReplace, (uchar*)&val,1);
-}
-
-void AppendProperty(Window w, Atom a, Atom type, unsigned long val)
-{
-	XChangeProperty(fl_display, w, a, type, 32, PropModeAppend, (uchar*)&val,1);
-}
-
-void DeleteProperty(Window w, Atom a, Atom type)
-{
-#warning DeleteProperty() smells like shit !
-	XDeleteProperty(fl_display, w, a);
-}
-*/
-
