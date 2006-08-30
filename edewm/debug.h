a0bc0cf23f3346781ac4aa8c80d87d2c30cd28f6
Sanel Zukan <karijes@users.sf.net>
2006-08-30 17:48:03 +0000

    Reverted old edewm version. Some files are missing !

---
+++ edewm/debug.h
@@ -0,0 +1,23 @@
+/*
+ * $Id$
+ *
+ * Edewm, window manager
+ * Part of Equinox Desktop Environment (EDE).
+ * Copyright (c) 2000-2006 EDE Authors.
+ *
+ * This program is licenced under terms of the 
+ * GNU General Public Licence version 2 or newer.
+ * See COPYING for details.
+ */
+
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+//#define DEBUG
+
+#include <stdio.h>
+#include <stdarg.h>
+extern void DBG(const char *str, ...);
+
+#endif
+
