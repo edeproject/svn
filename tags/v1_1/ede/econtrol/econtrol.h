c2636e0c9ecdb006fc4a7e9d4d46da64cfb359a4
Sanel Zukan <karijes@users.sf.net>
2006-09-02 20:54:11 +0000

    Tagged ede to 1.1

---
+++ tags/v1_1/ede/econtrol/econtrol.h
@@ -0,0 +1,33 @@
+/*
+ * $Id$
+ *
+ * Econtrol, control panel
+ * Part of Equinox Desktop Environment (EDE).
+ * Copyright (c) 2000-2006 EDE Authors.
+ *
+ * This program is licenced under terms of the 
+ * GNU General Public Licence version 2 or newer.
+ * See COPYING for details.
+ */
+#ifndef _ECONTROL_H_
+#define _ECONTROL_H_
+
+#include <efltk/Fl.h>
+#include <efltk/Fl_Locale.h>
+#include <efltk/Fl_Main_Window.h>
+#include <efltk/Fl_Menu_Bar.h>
+#include <efltk/Fl_Item_Group.h>
+#include <efltk/Fl_Item.h>
+#include <efltk/Fl_Group.h>
+#include <efltk/Fl_Box.h>
+#include <efltk/Fl_Tile.h>
+#include <efltk/Fl_Scroll.h>
+#include <efltk/Fl_Button.h>
+
+extern Fl_Main_Window* configPanelWindow;
+extern Fl_Menu_Bar* main_menubar;
+extern Fl_Group* main_view;
+extern Fl_Box* help_window;
+
+
+#endif
