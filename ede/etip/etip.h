ed88dc2f8cc7bed81378363c6324657576558161
Sanel Zukan <karijes@users.sf.net>
2007-03-27 10:02:22 +0000

    Copied ede to 1_x branch.

---
+++ ede/etip/etip.h
@@ -0,0 +1,23 @@
+/*
+ * $Id$
+ *
+ * Part of Equinox Desktop Environment (EDE).
+ * Copyright (c) 2000-2006 EDE Authors.
+ *
+ * This program is licenced under terms of the 
+ * GNU General Public Licence version 2 or newer.
+ * See COPYING for details.
+ */
+
+#ifndef etip_h
+#define etip_h
+#include <efltk/Fl.h>
+#include <efltk/Fl_Locale.h>
+#include <efltk/Fl_Window.h>
+#include <efltk/Fl_Box.h>
+#include <efltk/Fl_Check_Button.h>
+extern Fl_Check_Button* show_check;
+#include <efltk/Fl_Group.h>
+extern Fl_Box* tipsBox;
+#include <efltk/Fl_Button.h>
+#endif
