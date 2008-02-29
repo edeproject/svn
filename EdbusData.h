71a023f451c21cb30e7c81112e9760a10985884d
Sanel Zukan <karijes@users.sf.net>
2008-02-29 17:46:37 +0000

    Added !=, == operators. Added various is_TYPE() shortcuts for type() member

--- EdbusData.h
+++ EdbusData.h
@@ -44,7 +44,26 @@ class EdbusData {
 		EdbusData(const EdbusData& other); // essential for containers
 		~EdbusData();
 
-		EdbusDataType type();
+		EdbusDataType type() const;
+
+		bool is_byte(void) const { return type() == EDBUS_TYPE_BYTE; }
+		bool is_char(void) const { return is_byte(); }
+		bool is_bool(void) const { return type() == EDBUS_TYPE_BOOL; }
+		bool is_int16(void) const { return type() == EDBUS_TYPE_INT16; }
+		bool is_uint16(void) const { return type() == EDBUS_TYPE_UINT16; }
+		bool is_int32(void) const { return type() == EDBUS_TYPE_INT32; }
+		bool is_uint32(void) const { return type() == EDBUS_TYPE_UINT32; }
+		bool is_int64(void) const { return type() == EDBUS_TYPE_INT64; }
+		bool is_uint64(void) const { return type() == EDBUS_TYPE_UINT64; }
+		bool is_string(void) const { return type() == EDBUS_TYPE_STRING; }
+		bool is_object_path(void) const { return type() == EDBUS_TYPE_OBJECT_PATH; }
+		bool is_signature(void) const { return type() == EDBUS_TYPE_SIGNATURE; }
+		bool is_array(void) const { return type() == EDBUS_TYPE_ARRAY; }
+		bool is_struct(void) const { return type() == EDBUS_TYPE_STRUCT; }
+		bool is_variant(void) const { return type() == EDBUS_TYPE_VARIANT; }
+
+		bool operator==(const EdbusData& other);
+		bool operator!=(const EdbusData& other) { return !operator==(other); }
 };
 
 #endif
