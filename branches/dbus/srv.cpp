687f99b6db8b959dc865594a36b39a3e0b04d93b
Sanel Zukan <karijes@users.sf.net>
2008-03-07 15:43:06 +0000

    Dict marshalling stuff. Seems that works, but more checks are needed, of course. Some conde reordering. Candidate for more reorders...

--- branches/dbus/srv.cpp
+++ branches/dbus/srv.cpp
@@ -2,7 +2,9 @@
 #include <string.h>
 #include <Fl/Fl_Window.h>
 #include <Fl/Fl.h>
+
 #include "EdbusConnection.h"
+#include "EdbusDict.h"
 
 int signal_cb(const EdbusMessage* m, void*) {
 	printf("Got signal: %s : %s : %s\n", m->path(), m->interface(), m->member());
@@ -15,10 +17,26 @@ int method_cb(const EdbusMessage* m, void* w) {
 	printf("Message signature is: %s\n", m->signature());
 
 	if(strcmp(m->member(), "ChangeBackground") == 0) {
-		EdbusMessage::iterator it = m->begin();
-		int c = (*it).to_int16();
-		win->color(c);
-		win->redraw();
+		EdbusMessage::iterator it = m->begin(), it_end = m->end();
+		for(; it != it_end; ++it) {
+			if((*it).is_int16()) {
+				int c = (*it).to_int16();
+				win->color(c);
+				win->redraw();
+			}
+
+			if((*it).is_dict()) {
+				EdbusDict dict = (*it).to_dict();
+				EdbusDict::iterator dict_it = dict.begin(), dict_it_end = dict.end();
+
+				for(; dict_it != dict_it_end; ++dict_it) {
+					if((*dict_it).key.is_string())
+						printf("+++> key is %s\n", (*dict_it).key.to_string());
+					if((*dict_it).value.is_string())
+						printf("+++> value is %s\n", (*dict_it).value.to_string());
+				}
+			}
+		}
 	}
 
 	return 1;
