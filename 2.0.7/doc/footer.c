c6875c5552f37cd09fe87b576ee6c69c8eb907ef
Sanel Zukan <karijes@users.sf.net>
2009-02-11 13:53:39 +0000

    fixing

--- 2.0.7/doc/footer.c
+++
@@ -1,6 +0,0 @@
-/*  /\ 		$id$
- * / /\     Subproject: eFLTK
- * \  /\ 	This source code is released under GNU LGPL License
- *  \  / 	Copyright (c) EDE Team, 2000-today
- *   \/ 	Equinox Desktop Environment, http://ede.sf.net
- */
