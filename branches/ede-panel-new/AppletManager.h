b85d57aab28dc9b548da7e7ea13b571de95b8fd0
Sanel Zukan <karijes@users.sf.net>
2009-08-12 15:44:22 +0000

    Work, work...

---
+++ branches/ede-panel-new/AppletManager.h
@@ -0,0 +1,26 @@
+#ifndef __APPLETMANAGER_H__
+#define __APPLETMANAGER_H__
+
+#include <edelib/List.h>
+#include <edelib/String.h>
+#include "Applet.h"
+
+class  Panel;
+class  Fl_Widget;
+struct AppletData;
+
+typedef edelib::list<AppletData*> AList;
+typedef edelib::list<AppletData*>::iterator AListIter;
+
+class AppletManager {
+private:
+	AList applet_list;
+public:
+	~AppletManager();
+	bool load(const char *path);
+	void clear(void);
+	void fill_group(Panel *p);
+	void unfill_group(Panel *p);
+};
+
+#endif
--- branches/ede-panel-new/Panel.h
+++ branches/ede-panel-new/Panel.h
@@ -3,14 +3,24 @@
 
 #include <FL/Fl_Window.H>
 
+#define EDE_PANEL_CAST_TO_PANEL(obj) ((Panel*)(obj))
+
 class Panel : public Fl_Window {
 private:
 	Fl_Widget *clicked;
 	int        sx, sy;
+	bool       can_move_widgets;
+
+	void set_window_strut(int left, int right, int top, int bottom);
+
 public:
-	Panel(int w, int h, const char *l = 0) : Fl_Window(w, h, l), clicked(0), sx(0), sy(0) { }
+	Panel() : Fl_Window(300, 30), clicked(0), sx(0), sy(0), can_move_widgets(false) { box(FL_UP_BOX); }
 	~Panel() { }
-	int handle(int e);
+	int  handle(int e);
+	void show(void);
+
+	int panel_w(void) { return w(); }
+	int panel_h(void) { return h(); }
 };
 
 #endif
