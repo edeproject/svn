a7c0a1fe9a492a53f54019fdd36dcd7a6aca1d86
Sanel Zukan <karijes@users.sf.net>
2009-09-08 12:05:33 +0000

    Added "Application" category. .desktop files loading (not reading) code.

--- branches/ede-panel-new/applets/start-menu/XdgMenuReader.cpp
+++ branches/ede-panel-new/applets/start-menu/XdgMenuReader.cpp
@@ -1,12 +1,16 @@
+#include <sys/types.h>
+#include <dirent.h>
 #include <string.h>
 
 #include <edelib/TiXml.h>
 #include <edelib/Debug.h>
 #include <edelib/String.h>
+#include <edelib/StrUtil.h>
 #include <edelib/List.h>
 #include <edelib/Util.h>
 #include <edelib/Directory.h>
 #include <edelib/Config.h>
+#include <edelib/FileTest.h>
 
 #include "XdgMenuReader.h"
 #include "XdgMenuCategory.h"
@@ -15,14 +19,24 @@ EDELIB_NS_USING(String)
 EDELIB_NS_USING(Config)
 EDELIB_NS_USING(list)
 EDELIB_NS_USING(system_data_dirs)
+EDELIB_NS_USING(build_filename)
+EDELIB_NS_USING(file_test)
+EDELIB_NS_USING(str_ends)
+EDELIB_NS_USING(FILE_TEST_IS_DIR)
+
+#define DOT_OR_DOTDOT(base) (base[0] == '.' && (base[1] == '\0' || (base[1] == '.' && base[2] == '\0')))
 
 struct XdgMenu;
+struct MenuDesktopEntry;
 
 typedef list<String*> StrList;
 typedef list<String*>::iterator StrListIter;
 
 typedef list<XdgMenu*> XdgMenuList;
 typedef list<XdgMenu*>::iterator XdgMenuListIter;
+
+typedef list<MenuDesktopEntry*> DesktopEntryList;
+typedef list<MenuDesktopEntry*>::iterator DesktopEntryListIter;
 
 struct XdgMenu {
 	String *name;               /* menu name */
@@ -39,6 +53,12 @@ struct XdgMenu {
 	StrList merge_dirs;        /* <MergeDir> elements */
 
 	XdgMenuList submenus;      /* submenus */
+};
+
+/* each .desktop file in <AppDir> must have own Desktop-File Id */
+struct MenuDesktopEntry {
+	String name;
+	String desktop_file_id;
 };
 
 static void strlist_append(StrList &sl, const char *name) {
@@ -191,6 +211,81 @@ static void scan_menu_tag(TiXmlNode *elem, XdgMenuList& menus) {
 	menus.push_back(m);
 }
 
+static bool load_desktop_files_internal(const String *dir, DesktopEntryList &lst) {
+	DIR *dirp = opendir(dir->c_str());
+	if(!dirp)
+		return false;
+
+	dirent *dp;
+	String path;
+	bool   status = true;
+
+	while((dp = readdir(dirp)) != NULL) {
+		/* skip dots and (possibly) hidden files */
+		if(dp->d_name[0] == '.' || DOT_OR_DOTDOT(dp->d_name))
+			continue;
+
+		path = build_filename(dir->c_str(), dp->d_name);
+
+		if(file_test(path.c_str(), FILE_TEST_IS_DIR)) {
+			/* recurse if needed; the spec said we must */
+			status = load_desktop_files_internal(&path, lst);
+			continue;
+		}
+
+		/* it must have .desktop extension */
+		if(str_ends(path.c_str(), ".desktop")) {
+			MenuDesktopEntry *en = new MenuDesktopEntry;
+			en->name = path;
+			lst.push_back(en);	
+		}
+	}
+
+	closedir(dirp);
+	return status;
+}
+
+static void load_desktop_files(const String *dir, DesktopEntryList &lst) {
+	/* 
+	 * do not process further if failed, because 'lst' will be filled with
+	 * the content of previous directory
+	 */
+	if(!load_desktop_files_internal(dir, lst))
+		return;
+
+	const char *ptr;
+	MenuDesktopEntry *en;
+	DesktopEntryListIter it = lst.begin(), it_end = lst.end();
+	String tmp;
+
+	/* Now, scan the list and construct Desktop File Id and fill desktop_file_id for each member.
+	 * Desktop File Id is constructed by replacing '/' with '-' in subdirectory path, e.g.:
+	 *
+	 *   <AppDir>/foo.desktop           => foo.desktop
+	 *   <AppDir>/bla/foo.desktop       => bla-foo.desktop
+	 *   <AppDir>/bla/again/foo.desktop => bla-again-foo.desktop
+	 */
+	for(; it != it_end; ++it) {
+		en = *it;
+
+		if(en->name.length() < dir->length()) {
+			E_WARNING(E_STRLOC ": '%s' length is smaller than allowed. This entry will be skipped\n");
+			continue;
+		}
+
+		/* skip directory path + starting separator */
+		ptr = en->name.c_str() + dir->length();
+
+		while(*ptr == E_DIR_SEPARATOR)
+			ptr++;
+
+		tmp.assign(ptr);
+		tmp.replace(E_DIR_SEPARATOR, '-');
+
+		en->desktop_file_id = tmp;
+	}
+}
+
 void xdg_menu_load(void) {
 	TiXmlDocument doc;
 	//if(!doc.LoadFile("applets/start-menu/applications.menu")) {
@@ -205,6 +300,9 @@ void xdg_menu_load(void) {
 		return;
 	}
 
+	DesktopEntryList dlst;
+	StrListIter      sit, sit_end;
+
 	XdgMenuList menus;
 	scan_menu_tag(elem, menus);
 
@@ -212,9 +310,12 @@ void xdg_menu_load(void) {
 
 	for(; it != it_end; ++it) {
 		XdgMenu* m = (*it);
-		StrListIter sit = m->app_dirs.begin(), sit_end = m->app_dirs.end();
-		for(; sit != sit_end; ++sit)
+
+		sit = m->app_dirs.begin(), sit_end = m->app_dirs.end();
+		for(; sit != sit_end; ++sit) {
+			load_desktop_files(*sit, dlst);
 			E_DEBUG("appdir: %s\n", (*sit)->c_str());
+		}
 
 		sit = m->dir_dirs.begin(), sit_end = m->dir_dirs.end();
 		for(; sit != sit_end; ++sit)
@@ -227,6 +328,12 @@ void xdg_menu_load(void) {
 		sit = m->directory_stack.begin(), sit_end = m->directory_stack.end();
 		for(; sit != sit_end; ++sit)
 			E_DEBUG("directory: %s\n", (*sit)->c_str());
+	}
+
+	DesktopEntryListIter dit = dlst.begin(), dit_end = dlst.end();
+	for(; dit != dit_end; ++dit) {
+		E_DEBUG("%s => %s\n", (*dit)->name.c_str(), (*dit)->desktop_file_id.c_str());
+		delete *dit;
 	}
 
 	for(it = menus.begin(); it != it_end; ++it)
