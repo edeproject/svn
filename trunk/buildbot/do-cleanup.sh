f2fa37dcd63e437164dcc52c00db047209066c79
Sanel Zukan <karijes@users.sf.net>
2009-10-21 13:16:42 +0000

    cd into buildbot directory, so the script can be called with full path

--- trunk/buildbot/do-cleanup.sh
+++ trunk/buildbot/do-cleanup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # clean the files older than one week
 
+buildbot_dir="/var/www/buildbot"
+
+cd $buildbot_dir
+
 find ede* -mindepth 1 -type f -mtime +7 -exec rm {} \;
 find twistd.log* -mtime +7 -exec rm {} \;
