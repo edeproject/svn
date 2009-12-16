74cc7a526f42ec62bd3c4fecb70c3ef1cc7a25b4
Sanel Zukan <karijes@users.sf.net>
2009-12-16 14:40:37 +0000

    Added string-find.

--- branches/ede-scriptbus/string.cpp
+++ branches/ede-scriptbus/string.cpp
@@ -6,7 +6,7 @@
 #include "string.h"
 
 /* (string-split "string" "separator") */
-static pointer s_split(scheme* sc, pointer args) {
+static pointer s_string_split(scheme* sc, pointer args) {
 	pointer lst_ret = sc->NIL;
 
 	if(args == sc->NIL)
@@ -36,10 +36,41 @@ static pointer s_split(scheme* sc, pointer args) {
 	return lst_ret;
 }
 
+/* (string-find "string" "substr") */
+static pointer s_string_find(scheme* sc, pointer args) {
+	if(args == sc->NIL)
+		return sc->F;
+
+	const char *str, *sub, *p;
+
+	pointer a = sc->vptr->pair_car(args);
+	if(a == sc->NIL || !sc->vptr->is_string(a))
+		return sc->F;
+	str = sc->vptr->string_value(a);
+
+	args = sc->vptr->pair_cdr(args);
+	a = sc->vptr->pair_car(args);
+	if(a == sc->NIL || !sc->vptr->is_string(a))
+		return sc->F;
+	sub = sc->vptr->string_value(a);
+
+	p = strstr(str, sub);
+	if(!p) return sc->F;
+
+	/* return offset */
+	return mk_integer(sc, p - str);
+}
+
 void register_string_functions(scheme* sc) {
 	sc->vptr->scheme_define(
 		sc,
 		sc->global_env,
 		sc->vptr->mk_symbol(sc, "string-split"),
-		sc->vptr->mk_foreign_func(sc, s_split));
+		sc->vptr->mk_foreign_func(sc, s_string_split));
+
+	sc->vptr->scheme_define(
+		sc,
+		sc->global_env,
+		sc->vptr->mk_symbol(sc, "string-find"),
+		sc->vptr->mk_foreign_func(sc, s_string_find));
 }
